71820afbb045406e1756036ecb08be7c1bf49a4d
Omid Daliri <omiidnk02@gmail.com>
2026-09-06 22:20:35 +0330

    create SocialPromote Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import CategoriesListServer from "@/components/home/CategoriesListServer";
 import HeroSection from "@/components/home/HeroSection";
+import SocialPromote from "@/components/home/SocialPromote";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-22">
       <HeroSection />
       <CategoriesListServer />
+      <SocialPromote />
     </main>
   );
 }
